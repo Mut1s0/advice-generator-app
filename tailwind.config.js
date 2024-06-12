501eae6ede38d180fc878d4e3392da3e85c6ca85
Allan Musyoka <musyokaallan96@gmail.com>
2024-06-12 10:32:37 +0300

    Added images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,32 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        lightCyan: "hsl( 193, 38%, 86% )",
+        neonGreen: "hsl( 150, 100%, 66% )",
+
+        // Neutral
+        grayishBlue: "hsl( 217, 19%, 38% )",
+        darkGrayishBlue: "hsl( 217, 19%, 24% )",
+        darkBlue: "hsl( 218, 23%, 16% )"
+      },
+      fontFamily: {
+        manrope: [ "Manrope", "sans-serif" ]
+      },
+      fontWeight: {
+        manropeExtraBold: "800"
+      },
+    },
+    screens: {
+      xs: "344px",
+      ss: "375px",
+      sm: "425px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px"
+    }
   },
   plugins: [],
-}
-
+};
